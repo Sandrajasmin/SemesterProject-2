95e2d7166dcea861cda8cfa6e101ff1c60601c66
Sandra Jasmin <sandra-jasmin@hotmail.com>
2022-11-17 21:24:00 +0100

    add navbar responsive and with JS

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    fontFamily: {
+      'h1': ['Hind Guntur', 'sans-serif'],
+      'h2': ['Prompt', 'sans-serif'],
+      'body': ['Fira Sans', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
